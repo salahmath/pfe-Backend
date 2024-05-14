e4bafab13d64ffe3160a34549d5370b3d4428f8a
salah <salahmathlouthi000@gmail.com>
2024-05-14 12:02:43 +0100

    new commit 2/14/

--- config/config.js
+++ config/config.js
@@ -1,8 +1,9 @@
 const { default: mongoose } = require("mongoose")
 
 const db =()=>{
-  try{  const bdconnect = mongoose.connect(process.env.MONGO_URL);
+  try{  const bdconnect = mongoose.connect(process.env.MONGO_URL || 4000) ;
   console.log("base de donnée connecter ")
+  
 }catch(ereor){
 console.log("erreor de base de donnees ")
 }}
